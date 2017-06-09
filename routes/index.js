6ccbd86d4e2afcea63491ce2d3c91a9e9f192027
Thales <thalespizza@gmail.com>
2017-06-09 16:51:47 -0300

    Aula 7 completa

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-	const thales = { name: 'Thales', age: 27}
-	//res.send('Hey! It works!');
-	//res.json(thales);
-	//res.send(req.query.name);
-	//res.json(req.query);
-	res.render('hello', {
-		name: 'Thales',
-		dog: req.query.dog,
-		title: 'I love food!'
-	});
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
 	const reverse = [...req.params.name].reverse().join('');
