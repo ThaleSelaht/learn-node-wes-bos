f665fc10f1dbd70bec200f8d7a499c35b347b5ce
Thales <thalespizza@gmail.com>
2017-06-11 09:53:12 -0300

    Aula 8 completa

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,8 @@
 exports.myMiddleware = (req, res, next) => {
-  req.name = 'Wes';
+  req.name = 'Thales';
+  if (req.name === 'Thales') {
+    throw Error('That is a stupid name');
+  } 
   next();
 };
 exports.homePage = (req, res) => {
